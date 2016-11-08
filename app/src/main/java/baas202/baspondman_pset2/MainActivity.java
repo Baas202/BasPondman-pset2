ed39931fe11a154dc81045da1019155d24702265
Baas202 <b.pondman@gmail.com>
2016-11-08 01:54:47 +0100

    Initial commit

---
+++ app/src/main/java/baas202/baspondman_pset2/MainActivity.java
@@ -0,0 +1,13 @@
+package baas202.baspondman_pset2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
